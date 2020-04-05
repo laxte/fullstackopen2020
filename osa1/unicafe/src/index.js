1c4f6787bc3d09f6cdffd1f4e6f3a123fec1da87
Lauri Mikkola <lauri.mikkola@gmail.com>
2020-04-05 12:56:26 +0300

    Osa1, 1.6

---
+++ osa1/unicafe/src/index.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => {
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const incGood = () => {
+    setGood(good + 1)
+  }
+
+  const incBad = () => {
+    setBad(bad + 1)
+  }
+
+  const incNeutral = () => {
+    setNeutral(neutral + 1)
+  }
+
+  return (
+    <div>
+      <Feedback incGood={incGood} incBad={incBad} incNeutral={incNeutral} />
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </div>
+  )
+}
+
+const Feedback = (props) => {
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <button onClick={props.incGood}>good</button>
+      <button onClick={props.incNeutral}>netural</button>
+      <button onClick={props.incBad}>bad</button>
+    </div>
+  )
+}
+
+const Statistics = (props) => {
+  return (
+    <div>
+      <h1>statisticsk</h1>
+      <div>good {props.good}</div>
+      <div>neutral {props.neutral}</div>
+      <div>bad {props.bad}</div>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />,
+  document.getElementById('root')
+)
