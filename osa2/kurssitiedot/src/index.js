37eae0139695d8563c807c6275c3a1eca164d026
Lauri Mikkola <lauri.mikkola@gmail.com>
2020-04-07 11:03:08 +0300

    Osa2, 2.1

---
+++ osa2/kurssitiedot/src/index.js
@@ -0,0 +1,71 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => {
+  
+  const course = {
+    name: 'Half Stack application development',
+    id: 1,
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Course course={course} />
+    </div>
+  )
+}
+
+const Course = (props) => {
+  return (
+    <div>
+      <Header course={props.course.name} />
+      <Content parts={props.course.parts} />
+      <Total parts={props.course.parts} />
+    </div>
+  )
+}
+
+const Header = (props) => {
+  return (
+    <h1>{props.course}</h1>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part part={props.parts[0].name} exercises={props.parts[0].exercises} />
+      <Part part={props.parts[1].name} exercises={props.parts[1].exercises} />
+      <Part part={props.parts[2].name} exercises={props.parts[2].exercises} />
+    </div>
+  )
+}
+
+const Part = (props) => (
+  <p>
+  {props.part} {props.exercises}
+</p>
+)
+
+const Total = (props) => (
+  <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
+)
+
+ReactDOM.render(<App />, document.getElementById('root'))
