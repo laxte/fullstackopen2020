23e48743eeb9f94c0737ad0576fa9c84f81760a1
Lauri Mikkola <lauri.mikkola@gmail.com>
2020-04-25 11:28:16 +0300

    Osa2, 2.8

--- osa2/puhelinluettelo/src/components/App.js
+++ osa2/puhelinluettelo/src/components/App.js
@@ -1,43 +1,54 @@
 import React, { useState } from 'react'
 
 const App = () => {
-  const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
-  const [ newName, setNewName ] = useState('')
+  const [persons, setPersons] = useState([
+    {
+      name: 'Arto Hellas',
+      number: '12345'
+    }
+  ])
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
 
   const addPerson = (event) => {
     event.preventDefault()
-    console.log(event.target)
     const personObject = {
-      name : newName
+      name: newName,
+      number: newNumber
     }
     setPersons(persons.concat(personObject))
     setNewName('')
-  } 
+    setNewNumber('')
+  }
 
- const handleNewNameChange = (event) => {
-   console.log(event.target.value)
-   const newName = event.target.value
-   setNewName(newName)
-   const mathces = persons.filter(person => person.name === newName)
-   if (mathces.length > 0) {
-    window.alert(`${newName} is already added to phonebook`)
-    document.getElementById("mybutton").disabled = true
-   }
-   else {
-    document.getElementById("mybutton").disabled = false
-   }
- }
+  const handleNewNameChange = (event) => {
+    const newName = event.target.value
+    setNewName(newName)
+    const mathces = persons.filter(person => person.name === newName)
+    if (mathces.length > 0) {
+      window.alert(`${newName} is already added to phonebook`)
+      document.getElementById("mybutton").disabled = true
+    }
+    else {
+      document.getElementById("mybutton").disabled = false
+    }
+  }
 
+  const handleNumberChange = (event) => {
+    const newNumber = event.target.value
+    setNewNumber(newNumber)
+  }
 
   return (
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={addPerson}>
         <div>
-          name: <input value={newName} onChange={handleNewNameChange}/>
+          name: <input value={newName} onChange={handleNewNameChange} />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
         </div>
         <div>
           <button id="mybutton" type="submit">add</button>
@@ -53,9 +64,8 @@ const App = () => {
 const Persons = (props) => {
   return (
     props.persons.map(person => {
-      console.log(person)
       return (
-        <Person key={person.name} name={person.name} />
+        <Person key={person.name} name={person.name} number={person.number}/>
       )
     }
     )
@@ -64,7 +74,7 @@ const Persons = (props) => {
 
 const Person = (props) => {
   return (
-    <p> {props.name} </p>
+    <p> {props.name} {props.number}</p>
   )
 }
 
