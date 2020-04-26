cb4471e560c2ddbca54713e5d28f6285ecf62382
Lauri Mikkola <lauri.mikkola@gmail.com>
2020-04-26 10:12:03 +0300

    Osa2, 2.11

--- osa2/puhelinluettelo/src/components/App.js
+++ osa2/puhelinluettelo/src/components/App.js
@@ -1,16 +1,23 @@
-import React, { useState } from 'react'
+import React, { useState , useEffect } from 'react'
+import axios from 'axios'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [persons, setPersons] = useState('')
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
  
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }, [])
+
+
   const addPerson = (event) => {
     event.preventDefault()
     const personObject = {
@@ -86,7 +93,9 @@ const Filter = (props) => {
 }
 
 const Persons = (props) => {
-
+  if (props.persons.length < 1) {
+    return null
+  }
   let filteredPersons = props.persons.slice()
   if (props.filter.length !== 0) {
     filteredPersons = props.persons.filter(person =>
