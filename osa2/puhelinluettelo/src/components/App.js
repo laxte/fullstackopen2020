73afaef8620355581a757d6d0d5abbe332f49635
Lauri Mikkola <lauri.mikkola@gmail.com>
2020-05-19 08:50:34 +0300

    Osa2, 2.19

--- osa2/puhelinluettelo/src/components/App.js
+++ osa2/puhelinluettelo/src/components/App.js
@@ -6,6 +6,7 @@ const App = () => {
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
+  const [message, setMessage] = useState(null)
   const baseUrl = 'http://localhost:3001/persons'
 
 
@@ -27,11 +28,13 @@ const App = () => {
         setPersons(persons.concat(response))
         setNewName('')
         setNewNumber('')
+        showMessage(`Added ${personObject.name}`)
       })
   }
 
   const deletePerson = (event) => {
-    if (window.confirm(`Delete ${event.target.name} ?`)) {
+    const name = event.target.name
+    if (window.confirm(`Delete ${name} ?`)) {
       event.preventDefault()
       const id = parseInt(event.target.id)
       server.deletePerson(baseUrl, id)
@@ -40,6 +43,7 @@ const App = () => {
             return person.id !== id
           })
           setPersons(filteredPersons)
+          showMessage(`Deleted ${name}`)
         })
     }
   }
@@ -67,10 +71,17 @@ const App = () => {
     setFilter(newFilter)
   }
 
+  const showMessage = (messageToShow) => {
+    setMessage(messageToShow)
+    setTimeout(() => {
+      setMessage(null)
+    }, 5000)
+  }
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <Message message={message} />
       <Filter newFilter={filter} handleFilterChange={handleFilterChange} />
       <h2>add a new</h2>
       <PersonForm addPerson={addPerson} newName={newName} handleNewNameChange={handleNameChange}
@@ -79,6 +90,30 @@ const App = () => {
       <Persons persons={persons} filter={filter} deletePerson={deletePerson}/>
     </div>
   )
+}
+
+const Message = (props) => {
+  const messageStyle =
+  {
+    color: 'green',
+    background: 'lightgrey',
+    fontSize: 20,
+    borderStyle: 'solid',
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 10
+  }
+
+  if (props.message == null) {
+    return <div></div>
+  }
+  else {
+    return (
+      <div style={messageStyle}>
+        {props.message}
+      </div>
+    )
+  }
 }
 
 const PersonForm = (props) => {
